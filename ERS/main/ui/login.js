260857b31423bb33541598201ef97ccaa0d6dfda
HenryHsieh999 <henry.hsieh@revature.net>
2020-12-27 00:00:26 -0500

    login page initial setup complete

---
+++ ERS/main/ui/login.js
@@ -0,0 +1,29 @@
+async function loginSubmit(e){
+    e.preventDefault();
+
+    let username = document.getElementById("username-input").value
+    let password = document.getElementById("password-input").value
+
+    //enhanced object literals
+    const credentials = {
+        username,
+        password
+    }
+
+    try{
+
+        let res = await fetch("http://localhost:8080/firstservlets/login",{
+            method:"POST",
+            body: JSON.stringify(credentials),
+            headers:{
+                "Content-Type" : "application/json"
+            }
+        })
+
+        let user = await res.json()
+        console.log(user);
+    } catch(e) {
+        console.log(e);
+    }
+}
+document.getElementsByTagName("form")[0].addEventListener('submit', loginSubmit)
