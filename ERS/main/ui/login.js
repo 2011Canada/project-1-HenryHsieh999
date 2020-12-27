08a984fa7629f13c64098e2f36387831b3534cd3
HenryHsieh999 <henry.hsieh@revature.net>
2020-12-27 13:21:20 -0500

    added structure of UI

--- ERS/main/ui/login.js
+++ ERS/main/ui/login.js
@@ -11,19 +11,22 @@ async function loginSubmit(e){
     }
 
     try{
-
-        let res = await fetch("http://localhost:8080/firstservlets/login",{
+        let res = await fetch("http://localhost:8080/ERS/login",{
             method:"POST",
             body: JSON.stringify(credentials),
             headers:{
                 "Content-Type" : "application/json"
             }
         })
-
         let user = await res.json()
         console.log(user);
     } catch(e) {
         console.log(e);
     }
+    
 }
+
 document.getElementsByTagName("form")[0].addEventListener('submit', loginSubmit)
+
+
+
